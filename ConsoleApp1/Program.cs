c2d39bc1d7b3ad8594417c2c5c7cad91ffa75048
Nate <nathanvanderleest1@gmail.com>
2017-10-02 18:37:12 -0500

    Added method with Optional Parameters

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            TestMethod(3, 9);
+            TestMethod(notOptionalNumber: 9);
+            System.Console.ReadLine();
         }
+
+        #region Documentation
+        /// <summary>
+        /// Optional Arguments
+        /// </summary>
+        #endregion
+        public static void TestMethod(int notOptionalNumber, int optionalNumber = 10)
+        {
+            System.Console.WriteLine("Optional Number: " + optionalNumber + ", Not Optional Number: " + notOptionalNumber);
+        }
+
     }
 }
