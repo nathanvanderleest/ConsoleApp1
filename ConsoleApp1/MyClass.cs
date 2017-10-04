71d6dd60e7cd41af5f024d2d484d0ee0b92f65f4
Nate <nathanvanderleest1@gmail.com>
2017-10-03 19:04:22 -0500

    Created the MyClass.cs file and added constructors & Property

---
+++ ConsoleApp1/MyClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class MyClass
+    {
+        // If no constructor is declared, 
+        // then the basic one below is defined by the compiler
+        public MyClass()
+        {
+
+        }
+
+        // If only this constructor is declared, 
+        // then the complier will create the one above.
+        public MyClass(int number)
+        {
+            
+        }
+
+        private int _number;
+
+        public int Number
+        {
+            get { return _number;  }
+            set { _number = value;  }
+        }
+
+    }
+}
