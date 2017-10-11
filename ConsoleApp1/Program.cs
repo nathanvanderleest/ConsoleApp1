8be07da6e4e6934615b28d5dfed3541154214fd3
Nate <nathanvanderleest1@gmail.com>
2017-10-11 18:39:14 -0500

    Added some tests to the Main method for Dimensions struct

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Dimensions point;
+            point.Length = 3.6;
+            point.Width = 4.5;
+
+
+            Console.WriteLine("Length = " + point.Length);
+            Console.WriteLine("Width = " + point.Width);
+
+            Console.WriteLine("Diagonal = " + point.Diagonal);
+            Console.ReadLine();
         }
     }
 }
@@ -20,12 +30,20 @@ struct Dimensions
     public double Length;
     public double Width;
 
+    /// <summary>
+    /// Not sure how to use this constructor
+    /// </summary>
+    /// <param name="length"></param>
+    /// <param name="width"></param>
     public Dimensions(double length, double width)
     {
         Length = length;
         Width = width;
     }
 
+    /// <summary>
+    /// Distance Formula
+    /// </summary>
     public double Diagonal
     {
         get
