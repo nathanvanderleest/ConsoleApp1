5befab248c3f9726af40ce1770204b15f8f60daf
Nate <nathanvanderleest1@gmail.com>
2017-11-10 18:04:21 -0600

    asynchronous delegate & polling

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,13 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApp1
 {
     class Program
     {
+        public delegate int TakesAWhileDelegate(int data, int ms);
+
         static void Main(string[] args)
         {
+            // Synchronous method call
+            // TakesAWhile(1, 3000);
+
+            // asynchronous by using a delegate
+            TakesAWhileDelegate dl = TakesAWhile;
+
+            IAsyncResult ar = dl.BeginInvoke(1, 3000, null, null);
+            while(!ar.IsCompleted)
+            {
+                // do something else in the main thread
+                //Thread.Sleep(50);
+                Console.Write(".");
+                Thread.Sleep(50);
+            }
+            int result = dl.EndInvoke(ar);
+            Console.WriteLine("result: {0}", result);
+
+            Console.ReadKey();
+        }
+
+        static int TakesAWhile(int data, int ms)
+        {
+            Console.WriteLine("TakesAWhile started");
+            Thread.Sleep(ms);
+            Console.WriteLine("TakesAWhile completed");
+            return ++data;
         }
     }
 }
