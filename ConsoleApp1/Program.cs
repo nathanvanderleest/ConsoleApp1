604772e05bdd45102544dc8c2556e723f1613557
Nate <nathanvanderleest1@gmail.com>
2017-11-14 22:59:11 -0600

    Threads are foreground by default. Setting IsBackground properity to true makes it a background thread

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApp1
 {
@@ -10,6 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Background Threads
+            var t1 = new Thread(ThreadMain) { Name = "MyNewThread", IsBackground = true };
+            t1.Start();
+
+            Console.WriteLine("Main thread ending now.");
+            //Thread.Sleep(1000);
+        }
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("Thread {0} Started.", Thread.CurrentThread.Name);
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread {0} Completed.", Thread.CurrentThread.Name);
         }
     }
 }
