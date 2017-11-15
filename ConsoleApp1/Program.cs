3137bc1435a9212b64d43c7eb19f603b0dec5d48
Nate <nathanvanderleest1@gmail.com>
2017-11-14 21:35:06 -0600

    Creating a Thread and getting the thread id

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApp1
 {
@@ -10,6 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("running in the main thread, id: {0}", Thread.CurrentThread.ManagedThreadId);
+            var t1 = new Thread(ThreadMain);
+            t1.Start();
+            Console.WriteLine("Running in the main thread, id: {0}", Thread.CurrentThread.ManagedThreadId);
+
+            var t2 = new Thread(() => Console.WriteLine("running in a thread, id: {0}", Thread.CurrentThread.ManagedThreadId));
+            t2.Start();
+
+            Console.WriteLine("running in the main thread, id: {0}", Thread.CurrentThread.ManagedThreadId);
+            Console.ReadKey();
+        }
+
+        static void ThreadMain()
+        {
+            Console.WriteLine("running in the ThreadMain method, id: {0}", Thread.CurrentThread.ManagedThreadId);
         }
     }
 }
