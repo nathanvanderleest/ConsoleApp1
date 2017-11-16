6a462aacdd0e71fcef5e4bdb3b09f6c380641147
Nate <nathanvanderleest1@gmail.com>
2017-11-15 22:56:56 -0600

    First commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApp1
 {
@@ -10,6 +10,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            int nWorkerThreads;
+            int nCompletionPortThreads;
+            ThreadPool.GetMaxThreads(out nWorkerThreads, out nCompletionPortThreads);
+            Console.WriteLine("Max worker threads: {0}, " +
+                "I/O completion threads: {1} ",
+                nWorkerThreads, nCompletionPortThreads);
+
+
+            for(int i = 0; i < 5; i++)
+            {
+                ThreadPool.QueueUserWorkItem(JobForAThread);
+            }
+            Thread.Sleep(3000);
+            Console.ReadKey();
+        }
+
+        static void JobForAThread(object state)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("loop {0} running inside pooled thread {1}",
+                    i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(50);
+            }
         }
     }
 }
