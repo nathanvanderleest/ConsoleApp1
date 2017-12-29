f7a26d9004565edb1bdbd135ca76541629275cc6
Nate <nathanvanderleest1@gmail.com>
2017-12-28 22:37:34 -0600

    creating QuoteServer event source

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            if(!EventLog.SourceExists("QuoteServer"))
+            {
+                EventLog.CreateEventSource("QuoteServer","MyNewLog");
+                Console.WriteLine("CreateSourceEventLog");
+                Console.WriteLine("Exiting, execute again to write a to the log");
+                Console.ReadLine();
+                return;
+            }
+
+            EventLog eventLog = new EventLog();
+            eventLog.Source = "QuoteServer";
+            eventLog.WriteEntry("Write to event log.");
+            Console.WriteLine("Writting to the " + eventLog.Source + " event log source");
+
+            Console.ReadLine();
         }
     }
 }
